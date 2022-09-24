ba6b6f23a92c1a93638fdaf0dd39696d47a7f0c7
Hannes Lowette <Hannes.Lowette@axxes.com>
2022-09-24 10:24:47 +0200

    Exercice 1

--- Lesson2/Exercises/HackYourFuture.DotnetMasterclass.Lesson2.Exercises/Program.cs
+++ Lesson2/Exercises/HackYourFuture.DotnetMasterclass.Lesson2.Exercises/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var time = new JupiterTime();
+time.Hours = 8;
+time.Minutes = 40;
+
+PrintTime(time);
+
+void PrintTime(JupiterTime time)
+{
+    Console.WriteLine($"{time.Hours}:{time.Minutes:D2}");
+}
+
+class JupiterTime
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set; }
+}
