fce0d621bea9b8c10cdd16834db3c81a5bcab681
JorenVdkAxxes <joren.vandekerckhove@axxes.com>
2022-11-06 10:48:50 +0100

    Added the start project

---
+++ Lesson_security/SecureApiStartersProject/SecureApiStartersProject/Controllers/TodoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SecureApiStartersProject.DataLayer;
+
+namespace SecureApiStartersProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoRepository _todoRepository;
+
+        public TodoController(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTodoItem(string id)
+        {
+            var todoItem = await _todoRepository.Get(id);
+            return Ok(todoItem);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostTodoItem(string description)
+        {
+            var id = await _todoRepository.CreateAsync(description);
+            return Ok(id);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteTodoItem(string id)
+        {
+            await _todoRepository.Delete(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTodoItem(string id, bool completed)
+        {
+            await _todoRepository.Update(id, completed);
+            return Ok();
+        }
+    }
+}
---
+++ Lesson_security/SecureApiStartersProject/SecureApiStartersProject/Models/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿namespace SecureApiStartersProject.Models
+{
+    public class TodoItem
+    {
+        public TodoItem(string id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+
+        public TodoItem(string id, string description, bool completed)
+        {
+            Id = id;
+            Description = description;
+            Completed = completed;
+        }
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+    }
+}
